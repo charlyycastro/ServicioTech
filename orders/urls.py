886d250dcababcda40f85a972412d4b64a401a67
charlyycastro <163803208+charlyycastro@users.noreply.github.com>
2025-10-31 23:00:22 +0000

    Fix: app_name y rutas en orders/urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -1,10 +1,11 @@
+# orders/urls.py
 from django.urls import path
 from .views import OrderListView, order_create, OrderDetailView
 
+app_name = "orders"
 
-app_name = 'orders'
 urlpatterns = [
-path('', OrderListView.as_view(), name='list'),
-path('nuevo/', order_create, name='create'),
-path('<slug:folio>/', OrderDetailView.as_view(), name='detail'),
+    path("", OrderListView.as_view(), name="list"),
+    path("nuevo/", order_create, name="create"),
+    path("<slug:folio>/", OrderDetailView.as_view(), name="detail"),
 ]
